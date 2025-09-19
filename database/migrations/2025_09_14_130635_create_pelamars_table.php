859455837f2c26ef0dac2c4267bb9e9e6f6818d3
robzdd <2305050@student.polindra.ac.id>
2025-09-19 11:05:13 +0700

    database full (temp)

--- database/migrations/2025_09_14_130635_create_pelamars_table.php
+++ database/migrations/2025_09_14_130635_create_pelamars_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->uuid('id')->primary();
             $table->foreignUuid('user_id')->constrained('users')->onDelete('cascade');
             $table->foreignUuid('lowongan_id')->constrained('lowongan_pekerjaans')->onDelete('cascade');
-            $table->string('status')->default('pending'); // pending, diterima, ditolak
+            $table->enum('status_lowongan', ['menunggu', 'diterima', 'ditolak'])->default('menunggu'); // menunggu, diterima, ditolak
             $table->timestamps();
             $table->softDeletes();
         });
